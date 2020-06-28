2b0592d40a1c3996c9ea75800be0564b7a848dda
didiergaona <didiergaona@wposs.com>
2020-06-27 21:35:34 -0500

    modificación modelo

--- donante/models.py
+++ donante/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class DonanteModel(models.Model):
-    id = models.UUIDField(primary_key=True, auto_created=True)
     nombre = models.CharField(max_length=100)
     identificacion = models.CharField(max_length=100)
     correo = models.EmailField(max_length=100)
