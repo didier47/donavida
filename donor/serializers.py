354162ad1e9f8fe3227da433fd8f7ebf35a55f2b
didiergaona <didiergaona@wposs.com>
2020-07-17 11:43:00 -0500

    mejorando serializer de donor

--- donor/serializers.py
+++ donor/serializers.py
@@ -1,3 +1,4 @@
+from django_cassandra.common.utilities import Constants
 from rest_framework import serializers
 
 from donor.models import DonorModel
@@ -6,5 +7,19 @@ from donor.models import DonorModel
 class DonorSerializer(serializers.ModelSerializer):
     class Meta:
         model = DonorModel
-        fields = '__all__'
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['id',
+                  'last_login',
+                  'email',
+                  'password',
+                  'first_name',
+                  'last_name',
+                  'date_joined',
+                  'identification',
+                  'birth_date',
+                  'phone',
+                  'address']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'last_login': {'read_only': True, 'format': Constants.FORMAT_DATE_TIME},
+            'date_joined': {'read_only': True, 'format': Constants.FORMAT_DATE_TIME}
+        }
